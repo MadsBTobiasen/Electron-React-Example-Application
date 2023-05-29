dc7c8b824f9fb65d4cdc025a9aaa78c744a05319
Mads <Mads@DESKTOP-R1NEBSS>
2023-05-29 14:27:39 +0200

    Initial Commit

---
+++ src/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root");
+const root = createRoot(container)
+
+root.render(
+    <h1>Hello World - From ReactJs in Electron!</h1>
+)
